fea540fccf3c8eb219b7b0416e515e6aaa87124a
SrinivasAddla <srinivas.addla@ratnaglobaltech.com>
2023-12-27 08:55:58 +0530

    patterns programs

--- src/patterns/Pattern1.java
+++ src/patterns/Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1
 {
 	public static void main(String[] args)
 	{
-		Pattern1.pattern4();
+		Pattern1.pattern9();
 	}
 	
 	public static void pattern1() 
@@ -98,6 +98,38 @@ public class Pattern1
 			}
 			System.out.println();
 		}		
+	}
+	
+	public static void pattern8() {
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				if(j==i || j==n-i+1) {
+					System.out.print("*");
+				}
+				else {
+				System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void pattern9() {
+		int n=5;
+		for(int i=1;i<n;i++) {
+			for(int j=1;j<n;j++) {
+				if(i>=1 && i<n-1 && j==1 || i==n-2 && j>1) {
+					System.out.print("*");
+					}
+				else
+				{
+					System.out.print(" ");
+				}
+			
+			}
+			System.out.println();
+		}
 	}
 	
 	
